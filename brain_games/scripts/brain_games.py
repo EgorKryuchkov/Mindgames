185c63102070b81d344751044d08dbc28023ef36
Egor Kryuchkov <xopek17@mail.ru>
2023-06-25 21:15:56 +0300

    add prompt library and welcome_user function in brain_games

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+from brain_games import cli
 
 def greet():
-    print('Welcome to the Brain Games!')
+	print('Welcome to the Brain Games!')
 
 def main():
-    greet()
+	greet()
+	cli.welcome_user()
 
 if __name__ == '__main__':
-    main()
+	main()
